a386c81861a9de1c7f8471b45f02867899d3ed15
lesly <lesly.lasco@hotmail.fr>
2025-03-23 22:30:47 +0100

    MAJ Constructer

--- src/main/java/com/gdu/wacdo/entities/Affectation.java
+++ src/main/java/com/gdu/wacdo/entities/Affectation.java
@@ -4,19 +4,23 @@ import java.util.Date;
 
 public class Affectation {
 
+    private Long id;
     private Date dateDebut;
     private Date dateFin;
     private Fonctions fonctions;
     private Collaborateurs collaborateurs;
     private Restaurants restaurants;
 
-
-
-    public Affectation( Date dateDebut, Date dateFin, Fonctions fonctions, Collaborateurs collaborateurs, Restaurants restaurants,Long id) {
+    public Affectation(Long id, Date dateDebut, Date dateFin, Fonctions fonctions, Collaborateurs collaborateurs, Restaurants restaurants) {
+        this.id = id;
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
         this.fonctions = fonctions;
         this.collaborateurs = collaborateurs;
         this.restaurants = restaurants;
+    }
+
+    public Affectation() {
+
     }
 }
