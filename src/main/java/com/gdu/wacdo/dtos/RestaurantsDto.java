8d467a69aede81da1c2d5db7d52ec98ed0e2346f
lesly <lesly.lasco@hotmail.fr>
2025-03-23 22:34:54 +0100

    Ajout des getters setter

--- src/main/java/com/gdu/wacdo/dtos/RestaurantsDto.java
+++ src/main/java/com/gdu/wacdo/dtos/RestaurantsDto.java
@@ -1,8 +1,5 @@
 package com.gdu.wacdo.dtos;
 
-import lombok.Getter;
-
-@Getter
 public class RestaurantsDto {
 
     private String nom;
@@ -19,5 +16,37 @@ public class RestaurantsDto {
     }
 
     public RestaurantsDto() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
     }
 }
